8b54bd1a2221f9e5525d842dcdf15f5da6ff66fb
namutaka <namutaka+g@gmail.com>
2017-12-14 21:39:58 +0900

    DB設定部分ミス修正

--- pytter/settings.py
+++ pytter/settings.py
@@ -78,14 +78,15 @@ WSGI_APPLICATION = 'pytter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {}
-if os.environ.get('DATABASE_URL') == '':
-    DATABASES['default'] = {
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ATOMIC_REQUESTS': True,
     }
-else:
+}
+
+if os.environ.get('DATABASE_URL') != '':
     import dj_database_url
     db_from_env = dj_database_url.config()
     DATABASES['default'].update(db_from_env)
